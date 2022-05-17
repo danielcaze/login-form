fe68555e82ccc70168e083d818f8dfe673d1a651
Daniel Cazé <danielctlima@gmail.com>
2022-05-17 17:56:17 -0300

    added final touches

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ const elems = document.querySelectorAll(".active")
 
 let state = false;
 
+preventDef = evt => {
+  evt.preventDefault();
+}
+
 showPassword.addEventListener("click", () => {
   if (state) {
     for (i = 0; i < elems.length; i++) {
       elems[i].style.stroke = "#AFB6C2";
     }
-    password.focus()
     password.setAttribute("type", "password");
     state = false;
   } else {
     for (i = 0; i < elems.length; i++) {
       elems[i].style.stroke = "#FFC632";
     }
-    password.focus()
     password.setAttribute("type", "text");
     state = true;
   }
